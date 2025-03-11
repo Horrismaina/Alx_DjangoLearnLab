9c624419a424c7b166945166e640843fc5bc577e
Horrismaina <gacheruhorris@gmail.com>
2025-03-11 09:31:48 +0300

    correction8

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,32 +1,22 @@
 from django.db import models
 
-# Author model: Represents a book author.
 class Author(models.Model):
     name = models.CharField(max_length=100)
-
+    
     def __str__(self):
         return self.name
 
-# Book model: Represents a book written by an author.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # A book is written by one author.
-
-    def __str__(self):
-        return self.title
-
-# Library model: Represents a library that contains books.
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)  # A library can have many books.
-
+    address = models.CharField(max_length=200)
+    
     def __str__(self):
         return self.name
 
-# Librarian model: Represents the librarian assigned to a specific library.
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)  # Each library has one librarian.
-
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author, related_name='books')
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, null=True, blank=True)
+    
     def __str__(self):
-        return self.name
+        return self.title
