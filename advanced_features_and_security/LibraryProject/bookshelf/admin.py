8664e826062cdcc2bb8e92e858bfa89caf6761e9
Horrismaina <gacheruhorris@gmail.com>
2025-03-11 22:54:02 +0300

    correction18

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -16,29 +16,41 @@ class BookAdmin(admin.ModelAdmin):
     # Add field ordering
     ordering = ('title',)
 
-# Customizing the admin interface for CustomUser
-@admin.register(CustomUser)
+# CustomUserAdmin to manage CustomUser in the admin
 class CustomUserAdmin(UserAdmin):
-    # Display the following fields in the list view of the admin
-    list_display = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff')
+    # Define the fields to display in the list view
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
     
-    # Specify the fields to be used in the forms for adding and editing users
+    # Define the fields to search for
+    search_fields = ('username', 'email')
+    
+    # Add filters in the right sidebar
+    list_filter = ('is_staff', 'is_active')
+    
+    # Define the fieldsets to control how the fields are displayed in the admin form
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
     )
+    
+    # Define the fields for adding new users
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'date_of_birth', 'profile_photo', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    
+    ordering = ('username',)
 
-    # Add search functionality for CustomUser
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+# Register CustomUser with CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
 
-    # Add filter options for CustomUser
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
-
-# Register UserProfile with the admin
+# Register UserProfile for management in the admin
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'role')
     search_fields = ('user__username', 'role')
-    list_filter = ('role',)
+
