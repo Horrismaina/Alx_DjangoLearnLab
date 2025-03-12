3c4361f4dcc2a7afd78d1f9c3cbac65bda92ea87
Horrismaina <gacheruhorris@gmail.com>
2025-03-12 08:10:42 +0300

    correction21

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
+from django.views.decorators.csrf import csrf_protect
 from .models import Book
 
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -8,24 +9,28 @@ def book_list(request):
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
+@csrf_protect  # Ensuring CSRF protection for form submission
 def book_create(request):
     if request.method == "POST":
-        # Process form submission
+        # Process form submission securely
         pass
     return render(request, 'bookshelf/book_form.html')
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
+@csrf_protect  # Ensuring CSRF protection
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == "POST":
-        # Process form submission
+        # Process form submission securely
         pass
     return render(request, 'bookshelf/book_form.html', {'book': book})
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
+@csrf_protect  # CSRF protection on delete
 def book_delete(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == "POST":
         book.delete()
         return redirect('book_list')
     return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
+
