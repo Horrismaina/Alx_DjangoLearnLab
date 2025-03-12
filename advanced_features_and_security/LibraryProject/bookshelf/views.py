9a9498d2ea4d8c8483fcf8d93b9fbcc7a397c52d
Horrismaina <gacheruhorris@gmail.com>
2025-03-12 08:25:57 +0300

    correction24

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm  # Import the form
+from .forms import BookForm, ExampleForm  # Import the forms
 
+# Book list view
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
+# Book create view
 @permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
     if request.method == "POST":
@@ -19,6 +21,7 @@ def book_create(request):
         form = BookForm()
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
+# Book edit view
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
@@ -31,6 +34,7 @@ def book_edit(request, pk):
         form = BookForm(instance=book)
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
+# Book delete view
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def book_delete(request, pk):
     book = get_object_or_404(Book, pk=pk)
@@ -38,4 +42,15 @@ def book_delete(request, pk):
         book.delete()
         return redirect('book_list')
     return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
+
+# Example view using ExampleForm
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Handle the form submission
+            pass
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/example_form.html', {'form': form})
 
