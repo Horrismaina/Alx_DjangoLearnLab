8b7a7b3349082deb673a45ae7360032fac9f19e3
Horrismaina <gacheruhorris@gmail.com>
2025-03-23 23:32:20 +0300

    correction52

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm  # Import your custom form
 
-# Create your views here.
+# User registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# User profile view
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+@login_required
+def profile_update(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'blog/profile_update.html', {'u_form': u_form})
+
