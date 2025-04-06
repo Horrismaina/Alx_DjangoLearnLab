d730fa4046ca43dc2993fc1fd571cbfb1f6a67f0
Horrismaina <gacheruhorris@gmail.com>
2025-04-06 21:49:59 +0300

    correction85

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,10 +1,11 @@
-from rest_framework import viewsets, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import viewsets, permissions, generics
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from accounts.models import CustomUser
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -25,12 +26,13 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-class FeedView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+# Feed view for followed users' posts
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
 
-    def get(self, request):
-        user = request.user
-        followed_users = user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        user = self.request.user
+        following_users = user.following.all()  # Assuming `following` is a ManyToManyField in CustomUser
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
